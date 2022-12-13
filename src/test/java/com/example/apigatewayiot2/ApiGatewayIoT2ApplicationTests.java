8c7276af253dc0bbad7d3d861c5aa8680200f99a
Percy De La Rosa <a20192265@pucp.edu.pe>
2022-12-13 01:53:57 -0500

    primer commit

---
+++ src/test/java/com/example/apigatewayiot2/ApiGatewayIoT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayiot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayIoT2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
